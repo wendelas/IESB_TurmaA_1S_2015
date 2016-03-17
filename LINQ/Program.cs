b2fdefa8a44e643f1e090e204aae88b6966c0bda
Antonio Lopes Júnior <aclopesjr@hotmail.com>
2016-03-17 20:08:57 -0300

    Aula 4

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -15,47 +15,74 @@ namespace LINQ
         {
             CarregarJsons();
 
-            var filtroSimple1 = from f in funcionarios
-                                where f.Nome.StartsWith("A")
-                                select f;
-            //var filtroSimple = funcionarios
-            //    .Where((f) => f.Nome.StartsWith("A"));
+            //var filtroSimple1 = from f in funcionarios
+            //                    where f.Nome.StartsWith("A")
+            //                    select f;
+            ////var filtroSimple = funcionarios
+            ////    .Where((f) => f.Nome.StartsWith("A"));
 
-            foreach (var item in filtroSimple1)
-            {
-                Console.WriteLine($"{item.NomeCompleto}");
-            }
+            //foreach (var item in filtroSimple1)
+            //{
+            //    Console.WriteLine($"{item.NomeCompleto}");
+            //}
 
-            var filtroSimple2 = from f in funcionarios
-                                where f.Nome.StartsWith("A")
-                                select new
-                                {
-                                    Campo1 = f.Nome,
-                                    Campo2 = f.Sobrenome
-                                };
-            var filtroSimples2 = funcionarios
-                .Where((f) => f.Nome.StartsWith("A"))
-                .Select((f) => new
+            //var filtroSimple2 = from f in funcionarios
+            //                    where f.Nome.StartsWith("A")
+            //                    select new
+            //                    {
+            //                        Campo1 = f.Nome,
+            //                        Campo2 = f.Sobrenome
+            //                    };
+            //var filtroSimples2 = funcionarios
+            //    .Where((f) => f.Nome.StartsWith("A"))
+            //    .Select((f) => new
+            //    {
+            //        Campo1 = f.Nome,
+            //        Campo2 = f.Sobrenome
+            //    });
+            //foreach (var item in filtroSimple2)
+            //    Console.WriteLine($"{item.Campo1} {item.Campo2}");
+
+
+            //var filtro3 = (from f in funcionarios
+            //              group f by
+            //              f.Nome.First().ToString() into g
+            //              select new
+            //              {
+            //                  Chave = g.Key,
+            //                  Quantidade = g.Count()
+            //              }).OrderBy( g => g.Quantidade);
+            //foreach (var item in filtro3)
+            //{
+            //    Console
+            //        .WriteLine($"{item.Chave} - {item.Quantidade}");
+            //}
+
+            var filtro = from f in funcionarios
+                         group f by new
+                         {
+                             Faixa = ((DateTime.Now - f.DataAdmissao)
+                             .TotalDays / 365) > 3 ? "Senior" : "Junior"
+                         } into grp select
+                         new
+                         {
+                            Faixa = grp.Key, Quantidade = grp.Count()
+                         };
+
+            var filtr2 = funcionarios
+                .Select(f => new
                 {
-                    Campo1 = f.Nome,
-                    Campo2 = f.Sobrenome
+                    Faixa = ((DateTime.Now - f.DataAdmissao)
+                             .TotalDays / 365) > 3 ? "Senior" : "Junior"
+                })
+                .GroupBy( novoF => novoF.Faixa)
+                .Select( g => new
+                {
+                    Faixa = g.Key, Quantidade = g.Count()
                 });
-            foreach (var item in filtroSimple2)
-                Console.WriteLine($"{item.Campo1} {item.Campo2}");
-
-
-            var filtro3 = (from f in funcionarios
-                          group f by
-                          f.Nome.First().ToString() into g
-                          select new
-                          {
-                              Chave = g.Key,
-                              Quantidade = g.Count()
-                          }).OrderBy( g => g.Quantidade);
-            foreach (var item in filtro3)
+            foreach (var item in filtr2)
             {
-                Console
-                    .WriteLine($"{item.Chave} - {item.Quantidade}");
+                Console.WriteLine($"{item} - {item.Quantidade}");
             }
 
             Console.ReadLine();
